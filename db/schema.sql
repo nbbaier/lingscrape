edb33710661a95355ee78666388712aca21570ec
Nico Baier <nico.baier@gmail.com>
2024-03-30 11:44:41 -0500

    Add papers_test table and papers_search virtual table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 begin transaction;
 
-CREATE TABLE papers (
+CREATE TABLE IF NOT EXISTS papers (
   id INTEGER PRIMARY KEY,
   title TEXT,
   authors TEXT,
@@ -12,4 +12,27 @@ CREATE TABLE papers (
   downloads INTEGER
 );
 
+CREATE TABLE IF NOT EXISTS papers_test (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  authors TEXT,
+  date TEXT,
+  published_in TEXT,
+  keywords_raw TEXT,
+  abstract TEXT,
+  link TEXT,
+  downloads INTEGER
+);
+
+CREATE VIRTUAL TABLE IF NOT EXISTS papers_search USING fts5(id, title, keywords_raw, abstract);
+
+INSERT INTO
+  papers_search(id, title, keywords_raw, abstract)
+SELECT
+  id,
+  title,
+  keywords_raw,
+  abstract
+FROM
+  papers
 end transaction;
